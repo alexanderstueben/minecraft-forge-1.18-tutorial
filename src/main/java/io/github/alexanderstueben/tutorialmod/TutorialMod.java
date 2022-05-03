7b75c4532cb53db4889d21733f2662453e7257c1
alexanderstueben <alexander.stueben@dgc.org>
2022-05-03 13:52:34 +0200

    initial commit

---
+++ src/main/java/io/github/alexanderstueben/tutorialmod/TutorialMod.java
@@ -0,0 +1,12 @@
+package io.github.alexanderstueben.tutorialmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = TutorialMod.MODID)
+public class TutorialMod {
+    public static final String MODID = "tutorialmod";
+
+    public TutorialMod() {
+        
+    }
+}
